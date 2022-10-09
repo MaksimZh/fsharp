cf21370be2db0072423cb228b72cc923edf98610
Maksim Zholudev <maximzms@gmail.com>
2022-10-09 20:20:10 +0300

    Add list

---
+++ 34_list.fs
@@ -0,0 +1,18 @@
+// 34.1
+let upto n =
+    let rec fn = function
+      | (0, acc) -> acc
+      | (n, acc) -> fn(n - 1, n :: acc) 
+    fn(n, [])
+
+// 34.2
+let rec dnto = function
+  | 0 -> []
+  | n -> n :: dnto(n - 1)
+
+// 34.3
+let evenn n =
+    let rec fn = function
+      | (0, acc) -> acc
+      | (n, acc) -> fn(n - 1, n * 2 :: acc) 
+    fn(n, [])
