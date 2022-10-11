579919d6546d090d1151798fb8f154b25f9e416b
Maksim Zholudev <maximzms@gmail.com>
2022-10-11 20:14:23 +0300

    Add more lists

---
+++ 39_lists2.fs
@@ -0,0 +1,31 @@
+// 39.1
+let rec rmodd = function
+  | [] -> []
+  | [x] -> []
+  | first :: (second :: tail) -> second :: (rmodd tail)
+
+// 39.2
+let rec del_even = function
+  | [] -> []
+  | head :: tail when (head % 2 = 0) -> (del_even tail)
+  | head :: tail -> head :: (del_even tail)
+
+// 39.3
+let rec multiplicity = function
+  | (x, []) -> 0
+  | (x, head :: tail) when (head = x) -> 1 + multiplicity(x, tail)
+  | (x, _ :: tail) -> multiplicity(x, tail)
+
+// 39.4
+let rec split = function
+  | [] -> ([], [])
+  | [x] -> ([x], [])
+  | first :: (second :: tail) ->
+        let (a, b) = split tail
+        (first :: a, second :: b)
+
+// 39.5
+let rec zip = function
+  | ([], []) -> []
+  | (h1 :: t1, h2 :: t2) -> (h1, h2) :: zip(t1, t2)
+  | _ -> failwith "different lengths"
